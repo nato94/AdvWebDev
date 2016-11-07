ea04c151182431d8e033cd14c9471ecd14be76d3
nato94 <rc.nato94@gmail.com>
2016-11-07 09:02:06 -0500

    Added Assignment 4

---
+++ public_html/assignment4/app_server/controllers/form.js
@@ -0,0 +1,29 @@
+
+function formView(req, res) {
+    
+     res.render('form', { title: 'Form Page' });
+}
+
+function formPost(req, res) {
+    
+    if (req.method === 'POST') {
+       res.render('results', { 
+            title : 'Form Post Page',
+            name: req.body.name,
+            email: req.body.email,
+            comments: req.body.comments
+        });       
+    } else {
+         res.render('results', { 
+            title : 'Form Post Page',
+            name: 'No Name Found',
+            email: 'No Email Found',
+            comments: 'No Comments Found'
+        });
+    }   
+}
+
+/* Set the module class to have functions available for other files. */
+module.exports.formView = formView;
+module.exports.formPost = formPost;
+
